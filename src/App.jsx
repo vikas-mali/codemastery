6f90b765f8f1cedf7cfe1cfa44758e7469f45776
Vikas Mali <vikasmalione@gmail.com>
2023-08-30 11:58:13 +0000

    Update App file

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,7 @@ function App() {
 
   return (
     <>
-      <div>        
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} tr
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Vikas Mali</h1>
     </>
   )
 }
