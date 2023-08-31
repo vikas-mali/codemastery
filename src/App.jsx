e628435d42af8107c758ebdc651ae7c74e971bf5
Vikas Mali <vikasmalione@gmail.com>
2023-08-31 05:23:34 +0000

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import logoSrc from './assets/logo/Big Skillup logo-512.png'
 import './App.css'
+import Header from './components/common/header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
       <img src={logoSrc}/>
-      <h1>Coming Soon</h1>
+      <h1 className="text-3xl font-bold underline">Coming Soon</h1>      
     </>
   )
 }
